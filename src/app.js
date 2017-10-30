a6e301f3f9eb2b9a2fd00d543ff3be0860fce706
fmussap <fabiomussap@yahoo.com>
2017-10-30 18:42:01 +0000

    update 30/10

---
+++ src/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React, { PureComponent } from 'react'
+
+class App extends PureComponent {
+  render () {
+    return (
+      <div>
+        weather
+      </div>
+    )
+  }
+}
+
+export default App
